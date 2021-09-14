0d8eda7a5291663aac8ec7576eb7f43cc7df36a5
Ivaylo Novakov <inovakov@gmail.com>
2021-09-14 16:37:09 +0200

    Expose a Registry type that gives access to Skynet Registry.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # SkyDB
 
-This is a small SDK for [SkyDB](https://blog.sia.tech/skydb-a-mutable-database-for-the-decentralized-web-7170beeaa985).
+This is a small, unofficial SDK
+for [SkyDB](https://blog.sia.tech/skydb-a-mutable-database-for-the-decentralized-web-7170beeaa985). It also works
+for [Skynet Registry](https://blog.sia.tech/the-host-registry-building-dynamic-content-on-skynet-ade72ba6f30b) on which
+SkyDB is based.
--- skydb.go
+++ skydb.go
@@ -27,15 +27,19 @@ type SkyDBI interface {
 }
 
 type SkyDB struct {
+	r *Registry
+}
+
+type Registry struct {
 	Client *client.Client
 	sk     crypto.SecretKey
 	pk     crypto.PublicKey
 }
 
 // New creates a new SkyDB client with the given options.
-// If the options are empty we'll look for skyd at localhost:9980 and we'll use
-// "Sia-Agent" as user agent and we'll try to get the password for skyd from the
-// environment.
+// If the options are empty we'll look for skyd at localhost:9980, we'll use
+// "Sia-Agent" as user agent, and we'll try to get the password for skyd from
+// the environment.
 func New(sk crypto.SecretKey, pk crypto.PublicKey, opts client.Options) (*SkyDB, error) {
 	if reflect.DeepEqual(opts, client.Options{}) {
 		var err error
@@ -44,24 +48,27 @@ func New(sk crypto.SecretKey, pk crypto.PublicKey, opts client.Options) (*SkyDB,
 			return nil, errors.AddContext(err, "failed to get default client options")
 		}
 	}
-	skydb := &SkyDB{
+	r := &Registry{
 		Client: &client.Client{Options: opts},
 		sk:     sk,
 		pk:     pk,
+	}
+	skydb := &SkyDB{
+		r: r,
 	}
 	return skydb, nil
 }
 
 // Read retrieves from SkyDB the data that corresponds to the given key set.
 func (db SkyDB) Read(dataKey crypto.Hash) ([]byte, uint64, error) {
-	s, rev, err := db.registryRead(dataKey)
+	s, rev, err := db.r.RegistryRead(dataKey)
 	if err != nil && (strings.Contains(err.Error(), renter.ErrRegistryEntryNotFound.Error()) || strings.Contains(err.Error(), renter.ErrRegistryLookupTimeout.Error())) {
 		return nil, 0, ErrNotFound
 	}
 	if err != nil {
 		return nil, 0, errors.AddContext(err, "skydb failed to read from registry")
 	}
-	b, err := db.Client.SkynetSkylinkGet(s.String())
+	b, err := db.r.Client.SkynetSkylinkGet(s.String())
 	if err != nil && strings.Contains(err.Error(), renter.ErrRootNotFound.Error()) {
 		return nil, 0, ErrNotFound
 	}
@@ -73,44 +80,44 @@ func (db SkyDB) Read(dataKey crypto.Hash) ([]byte, uint64, error) {
 
 // Write stores the given `data` in SkyDB under the given key set.
 func (db SkyDB) Write(data []byte, dataKey crypto.Hash, rev uint64) error {
-	skylink, err := uploadData(db.Client, data)
+	skylink, err := uploadData(db.r.Client, data)
 	if err != nil {
 		return errors.AddContext(err, "failed to upload data")
 	}
-	_, err = db.registryWrite(skylink, dataKey, rev)
+	_, err = db.r.RegistryWrite(skylink, dataKey, rev)
 	if err != nil {
 		return errors.AddContext(err, "failed to write to the registry")
 	}
 	return nil
 }
 
-// registryWrite updates the registry entry with the given dataKey to contain
+// RegistryWrite updates the registry entry with the given dataKey to contain
 // the given skylink. Returns a SkylinkV2.
-func (db SkyDB) registryWrite(skylink string, dataKey crypto.Hash, rev uint64) (skymodules.Skylink, error) {
+func (r Registry) RegistryWrite(skylink string, dataKey crypto.Hash, rev uint64) (skymodules.Skylink, error) {
 	var sl skymodules.Skylink
 	err := sl.LoadString(skylink)
 	if err != nil {
 		return skymodules.Skylink{}, errors.AddContext(err, "failed to load skylink data")
 	}
 	// Update the registry with that link.
-	spk := types.Ed25519PublicKey(db.pk)
-	srv := modules.NewRegistryValue(dataKey, sl.Bytes(), rev, modules.RegistryTypeWithoutPubkey).Sign(db.sk)
-	err = db.Client.RegistryUpdate(spk, dataKey, srv.Revision, srv.Signature, sl)
+	spk := types.Ed25519PublicKey(r.pk)
+	srv := modules.NewRegistryValue(dataKey, sl.Bytes(), rev, modules.RegistryTypeWithoutPubkey).Sign(r.sk)
+	err = r.Client.RegistryUpdate(spk, dataKey, srv.Revision, srv.Signature, sl)
 	if err != nil {
 		return skymodules.Skylink{}, err
 	}
 	return skymodules.NewSkylinkV2(spk, dataKey), nil
 }
 
-// registryRead reads a registry entry and returns the SkylinkV2 it contains,
+// RegistryRead reads a registry entry and returns the SkylinkV2 it contains,
 // as well as the revision.
-func (db SkyDB) registryRead(dataKey crypto.Hash) (skymodules.Skylink, uint64, error) {
-	spk := types.Ed25519PublicKey(db.pk)
-	srv, err := db.Client.RegistryRead(spk, dataKey)
+func (r Registry) RegistryRead(dataKey crypto.Hash) (skymodules.Skylink, uint64, error) {
+	spk := types.Ed25519PublicKey(r.pk)
+	srv, err := r.Client.RegistryRead(spk, dataKey)
 	if err != nil {
 		return skymodules.Skylink{}, 0, errors.AddContext(err, "failed to read from the registry")
 	}
-	err = srv.Verify(db.pk)
+	err = srv.Verify(r.pk)
 	if err != nil {
 		return skymodules.Skylink{}, 0, errors.AddContext(err, "the value we read failed validation")
 	}
